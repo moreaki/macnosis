261566d653b39088df6ac1ebb771e4507fd032da
Roberto Nibali <roberto.nibali@engenius.ch>
2026-06-14 00:02:15 +0200

    Add Macnosis app icon

--- Sources/macnosis/App/MacnosisMain.swift
+++ Sources/macnosis/App/MacnosisMain.swift
@@ -1,8 +1,16 @@
+import AppKit
 import SwiftUI
 
 @main
 struct MacnosisMain: App {
     @StateObject private var model = MacnosisAppModel()
+
+    init() {
+        if let iconURL = Bundle.main.url(forResource: "MacnosisIcon", withExtension: "icns"),
+           let icon = NSImage(contentsOf: iconURL) {
+            NSApplication.shared.applicationIconImage = icon
+        }
+    }
 
     var body: some Scene {
         WindowGroup {
